b21a8da01909b76dbc398d9eacc4540139a2973d
Mika Attila <radiantstatue@gmail.com>
2014-07-30 15:18:18 +0200

    Hello SDL zordzman!

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
-#include <iostream>
+#include <SDL.h>
 
 int main() {
-    std::cout << "Hello zordzman world!\n";
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_SHOWN, &window, &renderer);
+    SDL_SetWindowTitle(window, "Hello zordzman++ world!");
+
+    bool quit = false;
+
+    while (!quit) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }

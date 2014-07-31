5285dcf069bdd2719b64ec47ebea6d8ae0d57070
Delu Anachebe <cjaf.javacakess@gmail.com>
2014-07-31 19:48:42 +0100

    Removed GLU(T) #includes, updated OSX_Makefile

--- src/Texture.hpp
+++ src/Texture.hpp
@@ -4,12 +4,8 @@
 
 #ifdef __APPLE__
 #  include <OpenGL/gl.h>
-#  include <OpenGL/glu.h>
-#  include <GLUT/glut.h>
 #else
 #  include <GL/gl.h>
-#  include <GL/glu.h>
-#  include <GL/glut.h>
 #endif
 
 class Texture {

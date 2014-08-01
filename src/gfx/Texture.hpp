4f6775a11a86e413f9beea74622f7a34420b7f1c
Mika Attila <radiantstatue@gmail.com>
2014-08-02 01:58:49 +0200

    Lal start adding doxygen documentation

--- src/gfx/Texture.hpp
+++ src/gfx/Texture.hpp
@@ -4,16 +4,24 @@
 
 #include <SDL_opengl.h>
 
+/// @brief Texture
 class Texture {
 public:
+    /// @brief Load the texture from a file
     bool loadFromFile(std::string const &filename);
+    /// @brief Get the width of the texture
     int getWidth() const;
+    /// @brief Get the height of the texture
     int getHeight() const;
+    /// @brief Get tile size. Dunno m8.
     int getTileSize() const;
+    /// @brief Bind a texture for subsequent drawing calls
     static void bind(Texture const &texture);
+    /// @brief Unbind the currently bound texture
     static void unbind();
     ~Texture();
     Texture(const Texture &) = delete;
+    /// @brief Construct the texture from a file
     Texture(std::string const &filename);
 
 private:

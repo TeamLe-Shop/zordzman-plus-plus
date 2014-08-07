528848d1820f00cd0fb7fb0a7145fabd4f524d63
MicroCode <cjaf.microcode@outlook.com>
2014-08-07 10:49:55 -0400

    Entity Header, but edited

--- src/Entity.h
+++ src/Entity.h
@@ -1,17 +1,13 @@
-#ifndef
-#define ENTITY_H
+#pragma once
 #include "Sprite.hpp"
 
-class Entity
-{
-	public:
-		int x; 
-		int y;
-		int w;
-		int h;
-		virtual void render();
-	private:
-		Sprite spr;
-	protected:
-}
-#endif
+class Entity {
+    public:
+    int x;
+    int y;
+    int w;
+    int h;
+    virtual void render();
+    private:
+    Sprite spr;
+};

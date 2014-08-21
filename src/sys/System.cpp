ca2fea5afdf1d11577caeddddc84bee9cf3829f6
Mika Attila <radiantstatue@gmail.com>
2014-08-22 00:59:00 +0200

    Lydl format

--- src/sys/System.cpp
+++ src/sys/System.cpp
@@ -3,25 +3,24 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_net.h>
-#include <sstream>
 #include <stdexcept>
+
+#include "format.h"
 
 namespace sys {
 
 System::System() {
-#define INIT_OR_THROW(cond, msg)                                               \
+#define INIT_OR_THROW(cond, ...)                                               \
     if (!(cond)) {                                                             \
-        std::ostringstream s;                                                  \
-        s << msg;                                                              \
-        throw std::runtime_error(s.str());                                     \
+        throw std::runtime_error(fmt::format(__VA_ARGS__));                    \
     }
 
     INIT_OR_THROW(SDL_Init(SDL_INIT_EVERYTHING) == 0,
-                  "Failed to initialize SDL: " << SDL_GetError());
+                  "Failed to initialize SDL: {}", SDL_GetError());
     INIT_OR_THROW(IMG_Init(IMG_INIT_PNG) == IMG_INIT_PNG,
-                  "Failed to initialize SDL_image: " << IMG_GetError());
-    INIT_OR_THROW(SDLNet_Init() != -1,
-                  "Failed to initialize SDL_net: " << SDLNet_GetError());
+                  "Failed to initialize SDL_image: {}", IMG_GetError());
+    INIT_OR_THROW(SDLNet_Init() != -1, "Failed to initialize SDL_net: {}",
+                  SDLNet_GetError());
 
 #undef INIT_OR_THROW
 }

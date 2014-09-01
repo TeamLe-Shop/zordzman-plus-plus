77d99a2d21ca5e2f3df4b888e50a2b8ee005cb99
Mika Attila <radiantstatue@gmail.com>
2014-09-01 12:04:44 +0200

    Replace a NULL with nullptr

--- server/main.cpp
+++ server/main.cpp
@@ -5,7 +5,7 @@
 int main() {
     IPaddress address;
 
-    if (SDLNet_ResolveHost(&address, NULL, 9001) < 0) {
+    if (SDLNet_ResolveHost(&address, nullptr, 9001) < 0) {
         fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
         exit(EXIT_FAILURE);
     }

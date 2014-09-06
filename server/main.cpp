7bfc4393e7b546f57001bb39acf646b2c30c3452
Delu Anachebe <cjaf.javacakess@gmail.com>
2014-09-06 09:20:40 +0100

    Use port 4544 pls

--- server/main.cpp
+++ server/main.cpp
@@ -2,10 +2,17 @@
 
 #include "lib/Server.hpp"
 
+#define PORT_NUMBER 4544 // The default port number.
+
 int main() {
     IPaddress address;
 
-    if (SDLNet_ResolveHost(&address, nullptr, 9001) < 0) {
+    // We could also load from a configuration file
+    // here. This would be done after this variable
+    // is assigned to PORT_NUMBER.
+    int port = PORT_NUMBER;
+
+    if (SDLNet_ResolveHost(&address, nullptr, port) < 0) {
         fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
         exit(EXIT_FAILURE);
     }

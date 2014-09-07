6692eb98b2a23ea5b9753fe78b9abd6e7b1729c3
Mika Attila <radiantstatue@gmail.com>
2014-09-07 18:54:29 +0200

    kaknado

--- client/entity/Eyenado.cpp
+++ client/entity/Eyenado.cpp
@@ -9,25 +9,26 @@ namespace mob {
 Eyenado::Eyenado(float x, float y) : Mob(x, y, 1.8f, SOUTH) { m_health = 45; }
 
 void Eyenado::render() const {
-    // i'm currently using Player.cpp as a drawing reference
-    SpriteSheet & sheet = Client::get().resources.getSheet("main");
-    sheet.setSpriteSize(16);
-    using drawingOperations::drawSpriteFromSheet;
-    // spin animation USE column 4
+    // Calculate frame
+    int idx = 0;
     if (ticks >= 0 && ticks < 30) {
-        drawSpriteFromSheet(sheet, 0, 4, m_x, m_y, 32, 32);
+        idx = 0;
     } else if (ticks >= 30 && ticks < 60) {
-        drawSpriteFromSheet(sheet, 1, 4, m_x, m_y, 32, 32);
+        idx = 1;
     } else if (ticks >= 60 && ticks < 90) {
-        drawSpriteFromSheet(sheet, 2, 4, m_x, m_y, 32, 32);
+        idx = 2;
     } else if (ticks >= 90 && ticks < 120) {
-        drawSpriteFromSheet(sheet, 3, 4, m_x, m_y, 32, 32);
+        idx = 3;
     } else if (ticks >= 120 && ticks < 150) {
-        drawSpriteFromSheet(sheet, 4, 4, m_x, m_y, 32, 32);
+        idx = 4;
     } else if (ticks >= 180 && ticks < 210) {
-        drawSpriteFromSheet(sheet, 5, 4, m_x, m_y, 32, 32);
+        idx = 5;
     }
-    // reset
+    // Draw it
+    SpriteSheet & sheet = Client::get().resources.getSheet("main");
+    using drawingOperations::drawSpriteFromSheet;
+    sheet.setSpriteSize(16);
+    drawSpriteFromSheet(sheet, idx, 4, m_x, m_y, 32, 32);
     sheet.setSpriteSize(8);
 }
 

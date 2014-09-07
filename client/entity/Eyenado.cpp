e58c98e4b9130445ae009814a244899deb6c93f0
Mika Attila <radiantstatue@gmail.com>
2014-09-07 18:34:34 +0200

    Jag theme song is gisu

--- client/entity/Eyenado.cpp
+++ client/entity/Eyenado.cpp
@@ -1,31 +1,30 @@
 #include "Eyenado.hpp"
 #include "gfx/drawingOperations.hpp"
 #include "Mob.hpp"
-#include "Game.hpp"
+#include "Client.hpp"
 
-using namespace mob;
+namespace client {
+namespace mob {
 
-Eyenado::Eyenado(float x, float y) {
-    : Mob(x, y, 1.8f, DOWN) {
-    m_health = 45;
-}
+Eyenado::Eyenado(float x, float y) : Mob(x, y, 1.8f, SOUTH) { m_health = 45; }
 
 void Eyenado::render() {
     // i'm currently using Player.cpp as a drawing reference
-    SpriteSheet &sheet = Game::get().resources.getSheet("main");
+    SpriteSheet & sheet = Client::get().resources.getSheet("main");
     sheet.setSpriteSize(16);
+    using drawingOperations::drawSpriteFromSheet;
     // spin animation USE column 4
     if (ticks >= 0 && ticks < 30) {
-        drawSpriteFromSheet(sheet, 0, 4, m_x, m_y 32, 32);
+        drawSpriteFromSheet(sheet, 0, 4, m_x, m_y, 32, 32);
     } else if (ticks >= 30 && ticks < 60) {
         drawSpriteFromSheet(sheet, 1, 4, m_x, m_y, 32, 32);
     } else if (ticks >= 60 && ticks < 90) {
         drawSpriteFromSheet(sheet, 2, 4, m_x, m_y, 32, 32);
     } else if (ticks >= 90 && ticks < 120) {
-       drawSpriteFromSheet(sheet, 3, 4, m_x, m_y, 32, 32);
-    } else if (ticks >= 120 && ticks < 150) { 
+        drawSpriteFromSheet(sheet, 3, 4, m_x, m_y, 32, 32);
+    } else if (ticks >= 120 && ticks < 150) {
         drawSpriteFromSheet(sheet, 4, 4, m_x, m_y, 32, 32);
-    } else if (ticks >= 180 && ticks < 210) { 
+    } else if (ticks >= 180 && ticks < 210) {
         drawSpriteFromSheet(sheet, 5, 4, m_x, m_y, 32, 32);
     } else if (ticks >= 240) {
         ticks = 0;
@@ -35,9 +34,11 @@ void Eyenado::render() {
     // reset
     sheet.setSpriteSize(8);
     // increase tick, for the animation
-    ticks++; 
+    ticks++;
 }
 
 void Eyenado::tick() {}
 
 void Eyenado::attack() {}
+}
+}
--- client/entity/Eyenado.hpp
+++ client/entity/Eyenado.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
+#include "Mob.hpp"
+
+namespace client {
+namespace mob {
 class Eyenado : public Mob {
 public:
     Eyenado(float x, float y);
     void render();
-    
+
 private:
     void tick();
     void attack();
     int ticks = 0;
 };
+}
+}

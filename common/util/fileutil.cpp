086bc6504af627ebfcd01d5cf980d7e9509e19c0
Delu Anachebe <cjaf.javacakess@gmail.com>
2014-11-02 18:32:03 +0000

    chinboy my one and only

---
+++ common/util/fileutil.cpp
@@ -0,0 +1,17 @@
+#include "fileutil.hpp"
+
+#include <iostream>
+
+namespace common {
+namespace util {
+namespace file {
+
+/// @brief Return the file name in a path string.
+std::string fileFromPath(std::string str) {
+    unsigned found = str.find_last_of("/\\");
+    return str.substr(found + 1);
+}
+
+} // namespace stream
+} // namespace util
+} // namespace common
---
+++ common/util/fileutil.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace common {
+namespace util {
+namespace file {
+
+/// @brief Return the file name in a path string.
+std::string fileFromPath(std::string path);
+
+} // namespace stream
+} // namespace util
+} // namespace common

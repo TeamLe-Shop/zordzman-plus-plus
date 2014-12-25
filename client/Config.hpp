f32358cf556385041cc095f42519c38ade33b9f1
Delu Anachebe <cjaf.javacakess@gmail.com>
2014-12-25 13:10:35 +0000

    Remove outdated comment

--- client/Config.hpp
+++ client/Config.hpp
@@ -7,10 +7,8 @@ namespace client {
 /// The configuration structure that holds
 ///  different config variables.
 struct Config {
-    std::string host = "localhost"; // localhost doesn't work
-                                    // IPv6 not supported.
+    std::string host = "gatsan.ddns.net";
     int port = 4544;
-
-    std::string name = "SneakySnake";
+    std::string name = "gatsan";
 };
 } // namespace client

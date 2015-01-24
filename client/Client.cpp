031aa36f32e23ab61cf9ba59469c03e328529756
Mika Attila <radiantstatue@gmail.com>
2015-01-24 10:40:39 +0100

    Make Client.cpp build on windoze

--- client/Client.cpp
+++ client/Client.cpp
@@ -13,6 +13,8 @@
 #include <winsock2.h>
 #include <windows.h>
 #include <ws2tcpip.h>
+#include <tchar.h>
+#include <strsafe.h>
 #else
 #include <dirent.h>
 #include <netdb.h>
@@ -219,6 +221,72 @@ void Client::exec() {
     }
 }
 
+namespace {
+std::vector<std::string> getDirectoryContents(std::string const & path) {
+    std::vector<std::string> entries;
+#ifdef _WIN32
+    WIN32_FIND_DATA ffd;
+    TCHAR szDir[MAX_PATH];
+    size_t length_of_arg;
+    HANDLE hFind = INVALID_HANDLE_VALUE;
+    DWORD dwError = 0;
+
+    // Check that the input path plus 3 is not longer than MAX_PATH.
+    // Three characters are for the "\*" plus NULL appended below.
+
+    StringCchLength(path.c_str(), MAX_PATH, &length_of_arg);
+
+    if (length_of_arg > (MAX_PATH - 3)) {
+        throw std::runtime_error("Directory path is too long.");
+    }
+
+    // Prepare string for use with FindFile functions.  First, copy the
+    // string to a buffer, then append '\*' to the directory name.
+
+    StringCchCopy(szDir, MAX_PATH, path.c_str());
+    StringCchCat(szDir, MAX_PATH, TEXT("\\*"));
+
+    // Find the first file in the directory.
+
+    hFind = FindFirstFile(szDir, &ffd);
+
+    if (INVALID_HANDLE_VALUE == hFind) {
+        throw std::runtime_error("Invalid handle.");
+    }
+
+    // List all the files in the directory with some info about them.
+
+    do {
+        if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            // Do something for directories?
+        } else {
+            entries.push_back(ffd.cFileName);
+        }
+    } while (FindNextFile(hFind, &ffd) != 0);
+
+    dwError = GetLastError();
+    if (dwError != ERROR_NO_MORE_FILES) {
+        throw std::runtime_error("Some kind of error, dunno");
+    }
+
+    FindClose(hFind);
+#else
+    DIR * dir;
+    struct dirent * ent;
+
+    if ((dir = opendir(path.c_str())) == nullptr) {
+        throw std::runtime_error(
+            fmt::format("Couldn't open directory \"{}\"", "resources/levels"));
+    }
+
+    while ((ent = readdir(dir)) != nullptr) {
+        entries.push_back(ent->d_name);
+    }
+#endif
+    return entries;
+}
+}
+
 void Client::checkForMap(std::string map, std::string hash) {
 #ifndef _WIN32 // YEAH FUCK THIS ON  WINDOWS
     using namespace common::util::file;
@@ -228,21 +296,14 @@ void Client::checkForMap(std::string map, std::string hash) {
     m_map_hash = hash;
 
     // The client is going to now look for that map file.
-    DIR * dir;
-    struct dirent * ent;
+    auto entries = getDirectoryContents("resources/levels/");
 
-    if ((dir = opendir("resources/levels/")) == nullptr) {
-        throw std::runtime_error(
-            fmt::format("Couldn't open directory \"{}\"", "resources/levels"));
-    }
-
-    while ((ent = readdir(dir)) != nullptr) {
+    for (auto name : entries) {
         // Does the map hash match the file name?
-        if (!strcmp(ent->d_name, hash.c_str())) {
+        if (name == hash) {
             // Open a stream to the file.
-            std::ifstream mapfile(
-                fmt::format("resources/levels/{}", ent->d_name),
-                std::ios::binary | std::ios::in);
+            std::ifstream mapfile(fmt::format("resources/levels/{}", name),
+                                  std::ios::binary | std::ios::in);
 
             // Read all data...
             std::vector<char> mapdata =
@@ -251,7 +312,7 @@ void Client::checkForMap(std::string map, std::string hash) {
             MD5 md5;
             // Generate a hash from the map data
             md5.add(mapdata.data(), mapdata.size());
-            if (!strcmp(md5.getHash().c_str(), ent->d_name)) {
+            if (md5.getHash() == name) {
                 found_match = true;
                 m_level = Level(hash);
             } else {

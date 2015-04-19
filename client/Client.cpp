e9a7de7759c035d4d14a4c39d9f3ff09b586a32c
Mika Attila <radiantstatue@gmail.com>
2015-04-19 21:41:50 +0200

    Something something debugSystem don't try to use null component

--- client/Client.cpp
+++ client/Client.cpp
@@ -70,12 +70,15 @@ void debugSystem(entity::EntityCollection * coll, entity::Entity & ent) {
     auto character = COMPONENT(ent, entity::CharacterComponent);
     auto render    = COMPONENT(ent, entity::RenderComponent);
 
+    auto spriteinfo = render ? render->m_sprite.get() : "[No render component]";
+    auto alphainfo = render ? render->m_alpha.get() : 0.f;
+
     fmt::print("Frame: #{}, Entity ID: #{}:\n"
                "\tCharacter: Name: \"{}\", Health: {}, Max Health: {}\n"
                "\tRender Info: Sprite: \"{}\", Alpha: {:f}\n",
                 coll->getFrame(), ent.getID(), character->m_name.get(),
                 character->m_health.get(), character->m_max_health.get(),
-                render->m_sprite.get(), render->m_alpha.get());
+                spriteinfo, alphainfo);
 }
 }
 

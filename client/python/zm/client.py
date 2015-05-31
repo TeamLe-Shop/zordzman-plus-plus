288505643613c2c5c49a2c682d3ac7da527025b2
Oliver Ainsworth <ottajay@googlemail.com>
2015-05-31 16:22:43 +0100

    Add validation for JSON messages

--- client/python/zm/client.py
+++ client/python/zm/client.py
@@ -3,8 +3,58 @@ import logging
 import socket
 import threading
 
+import formencode
+
 
 log = logging.getLogger(__name__)
+
+
+class PlayerStateSchema(formencode.Schema):
+
+    id = formencode.validators.Int()
+    component = formencode.validators.String()
+    field = formencode.validators.String()
+    value = formencode.Any(validators=[
+        formencode.validators.ConfirmType(type=list),
+        formencode.validators.ConfirmType(type=dict),
+        formencode.validators.ConfirmType(type=int),
+        formencode.validators.ConfirmType(type=float),
+        formencode.validators.ConfirmType(type=str),
+    ])
+
+
+MESSAGE_TYPES = {
+    "entity.state": PlayerStateSchema,
+    "player.id": formencode.validators.Int(),
+}
+
+
+def validate_entity(message, state, validator):
+    type_ = message["type"]
+    entity = message["entity"]
+    try:
+        message["entity"] = MESSAGE_TYPES[type_].to_python(entity)
+    except formencode.Invalid as invalid:
+        raise formencode.Invalid(
+            "entity: " + invalid.msg,
+            message,
+            state,
+            error_dict={"entity": invalid},
+        )
+
+
+class MessageSchema(formencode.Schema):
+
+    type = formencode.validators.OneOf(list(MESSAGE_TYPES.keys()))
+    entity = formencode.Any(validators=[
+        formencode.validators.ConfirmType(type=list),
+        formencode.validators.ConfirmType(type=dict),
+        formencode.validators.ConfirmType(type=int),
+        formencode.validators.ConfirmType(type=float),
+        formencode.validators.ConfirmType(type=str),
+    ])
+    chained_validators = [
+        formencode.schema.SimpleFormValidator(validate_entity)]
 
 
 class ZMClient(threading.Thread):
@@ -36,6 +86,17 @@ class ZMClient(threading.Thread):
             i += 1
         return messages
 
+    def _validate_messages(self, messages):
+        validated = []
+        for message in messages:
+            try:
+                message = MessageSchema.to_python(message)
+            except formencode.Invalid as exc:
+                log.warning("Invalid message: %s", exc)
+            else:
+                validated.append(message)
+        return validated
+
     def run(self):
         self._connect("127.0.0.1", 9001)
         while True:
@@ -48,5 +109,5 @@ class ZMClient(threading.Thread):
             if messages:
                 log.debug("%i new messages; %i bytes remain "
                           "in the buffer", len(messages), len(self._buffer))
-            log.debug(messages)
+            messages = self._validate_messages(messages)
         log.info("Client exiting")

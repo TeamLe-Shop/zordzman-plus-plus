92f03eccebb56851d1b31f42d8a45fb6171c0326
Oliver Ainsworth <ottajay@googlemail.com>
2015-05-31 14:51:33 +0100

    Move Python stuff into 'python' namespace

--- client/main.cpp
+++ client/main.cpp
@@ -1,5 +1,3 @@
-#include <Python.h>
-
 #include "Client.hpp"
 
 #include <stdexcept>
@@ -9,39 +7,14 @@
 #include "format.h"
 #include "Config.hpp"
 #include "HUD.hpp"
+#include "python/python.hpp"
 
 using namespace client;
 
 
-void start_zm_client() {
-    Py_InitializeEx(0);
-    PyList_Insert(PySys_GetObject("path"),
-                  0, PyUnicode_FromString("../client/python/"));
-    auto zm = PyImport_ImportModule("zm");
-    if (!zm) {
-        // TODO: Check error and die
-    }
-    auto zm_main = PyObject_GetAttr(zm, PyUnicode_FromString("main"));
-    if (!zm_main || !PyCallable_Check(zm_main)) {
-        std::cout << "Python entry point (zm.main) does not exist or is not callable";
-    }
-    auto zm_main_rv = PyObject_CallFunction(zm_main, "");
-    if (!zm_main_rv) {
-        auto exc = PyErr_Occurred();
-        if (exc) {
-            PyErr_Print();
-        }
-    } else {
-        fprintf(stderr, "zm.main() -> ");
-        PyObject_Print(zm_main_rv, stderr, Py_PRINT_RAW);
-        fprintf(stderr, "\n");
-    }
-    Py_Finalize();
-}
-
-
 int main(int argc, char * argv[]) {
-    start_zm_client();
+    python::initialise();
+    python::finalise();
     return 0;
     try {
         Config cfg("config/config.json");

be9f4fb600d20813b49942e4938dc706f9443367
Oliver Ainsworth <ottajay@googlemail.com>
2015-05-31 15:12:35 +0100

    Send magic number after connecting to server

--- client/python/zm/client.py
+++ client/python/zm/client.py
@@ -1,4 +1,5 @@
 import logging
+import socket
 import threading
 
 
@@ -7,9 +8,19 @@ log = logging.getLogger(__name__)
 
 class ZMClient(threading.Thread):
 
+    MAGIC_NUMBER = b"\xCA\xC3\x55\x01"
+
     def __init__(self):
         super().__init__(name=self.__class__.__name__ + "Thread")
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def _connect(self, host, port):
+        log.info("Connecting to %s:%i", host, port)
+        self._socket.connect((host, port))
+        self._socket.sendall(self.MAGIC_NUMBER)
 
     def run(self):
-        log.info("Client running")
+        self._connect("127.0.0.1", 9001)
+        while True:
+            break
         log.info("Client exiting")
